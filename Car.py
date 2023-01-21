c240f1b679b9b19d496ace288e969e099d295804
KishorSura <117828152+KishorSura@users.noreply.github.com>
2023-01-21 19:49:03 +0530

    Add files via upload

---
+++ Car.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[24]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[5]:
+
+
+df=pd.read_csv("C:/Users/DELL/Downloads/2. Cars Data1.csv")
+
+
+# In[6]:
+
+
+df.head()
+
+
+# In[7]:
+
+
+df.shape
+
+
+# In[8]:
+
+
+#fill the null values with mean of the respective column
+df.isnull().sum()
+
+
+# In[9]:
+
+
+#lets drop
+new_df1=df.dropna(how="all")
+
+
+# In[10]:
+
+
+new_df1.isnull().sum()
+
+
+# In[11]:
+
+
+#next fill the null values with mean
+df1=new_df1.fillna({"Cylinders":new_df1.Cylinders.mean()})
+df1
+
+
+# In[12]:
+
+
+df1.isnull().sum()
+
+
+# In[13]:
+
+
+#2 Check oyt different types of dataset and count the occurrence 
+df1.head(2)
+
+
+# In[14]:
+
+
+df1["Make"].value_counts()
+
+
+# In[15]:
+
+
+#show all the records of origin where condition is Asia or Europe 
+df1.head()
+
+
+# In[16]:
+
+
+df1[(df1["Origin"]=="Asia")|(df1["Origin"]=="Europe")]
+
+
+# In[17]:
+
+
+df1[df1["Origin"].isin(["Asia","Europe"])]
+
+
+# In[18]:
+
+
+#remove all records where weight is above 4000
+df1[~(df1["Weight"]>4000)]
+
+
+# In[19]:
+
+
+#increase the value of  coulmn "MPG_Highway" by 3
+df1["MPG_City"]=df1["MPG_City"].apply(lambda x:x+3)
+
+
+# In[20]:
+
+
+df1
+
+
+# In[36]:
+
+
+#graphical representation of count of make
+
+a=df1["Make"].value_counts()
+
